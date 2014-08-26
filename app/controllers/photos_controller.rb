b250b554fb5b96e6905e6fa06bb7da1280cd4b60
hqcasanova <hqcasanova@users.noreply.github.com>
2014-08-26 18:16:53 -0400

    Image listing at homepage and image addition implemented

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,19 @@
 class PhotosController < ApplicationController
   def index
+    @photos = Photo.all
   end
 
   def new
+  end
+
+  def create
+    @photo = Photo.create(photo_params)
+    redirect_to photos_path
+  end
+
+  #Honor security restrictions
+  private
+  def photo_params 
+    params.require(:photo).permit(:title, :author, :url)
   end
 end
