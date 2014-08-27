b75e12c396cbda51c1216b7f724b0533482f4e93
hqcasanova <hqcasanova@users.noreply.github.com>
2014-08-26 22:13:14 -0400

    Photo deletion and details edition implemented

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,11 +3,21 @@ class PhotosController < ApplicationController
     @photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
   def new
   end
 
   def create
     @photo = Photo.create(photo_params)
+    redirect_to photos_path
+  end
+
+  def destroy
+    @photo = Photo.find(params[:id])
+    @photo.destroy
     redirect_to photos_path
   end
 
