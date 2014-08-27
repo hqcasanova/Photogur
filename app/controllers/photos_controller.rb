2aae0b669a331cef1eac2c21bcbe767c7cd4ed7e
hqcasanova <hqcasanova@users.noreply.github.com>
2014-08-26 23:58:57 -0400

    Photo edit finished up

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -15,6 +15,19 @@ class PhotosController < ApplicationController
     redirect_to photos_path
   end
 
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+    if @photo.update_attributes(photo_params)
+      redirect_to photo_path
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @photo = Photo.find(params[:id])
     @photo.destroy
